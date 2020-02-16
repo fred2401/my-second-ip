4c43864ad4dd8b17caf4c72ef9e57f607f395ab5
frederick asacha <fredasacha2@gmail.com>
2020-02-16 20:37:41 +0300

    created variables for the age calculation.

---
+++ the Ip/js/script.js
@@ -0,0 +1,3 @@
+var yearCC=parseInt(prompt("enter the first 2 digits of year you were born"));
+var yearYY=parseInt(prompt("enter the last 2 digits of year you were born"));
+var month=(prompt("enter the month you were born"));
