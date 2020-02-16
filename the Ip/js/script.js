77be8cdd2e79196efa0d3a38fa729c0e6d151927
frederick asacha <fredasacha2@gmail.com>
2020-02-16 20:39:31 +0300

    added an array for the months amd a code to get the numerical value of the month.

--- the Ip/js/script.js
+++ the Ip/js/script.js
@@ -1,3 +1,5 @@
 var yearCC=parseInt(prompt("enter the first 2 digits of year you were born"));
 var yearYY=parseInt(prompt("enter the last 2 digits of year you were born"));
 var month=(prompt("enter the month you were born"));
+var months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "november", "december"];
+var monthMM=parseInt(months.indexOf(month))+1;
