4cb7f4cdfbf396d103636c9b5365f94f18992a00
frederick asacha <fredasacha2@gmail.com>
2020-02-16 20:43:02 +0300

    added a function to calculate the day.

--- the Ip/js/script.js
+++ the Ip/js/script.js
@@ -4,3 +4,7 @@ var month=(prompt("enter the month you were born"));
 var months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "november", "december"];
 var monthMM=parseInt(months.indexOf(month))+1;
 var date=parseInt(prompt("enter the date you were born"));
+
+var day=function(yearYY, monthMM, date, yearCC){
+    return -1*(((yearCC/4) -2*yearCC-1)+((5*yearYY/4))+((26*(monthMM+1)/10))+date)%7;
+};
