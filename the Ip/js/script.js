d7d936a09ee5de84536d8e67da894c7d794686d0
frederick asacha <fredasacha2@gmail.com>
2020-02-17 12:52:16 +0300

    added forms

--- the Ip/js/script.js
+++ the Ip/js/script.js
@@ -1,40 +1,31 @@
-var test=prompt("wanna discover your akan name?");
-if (test==="yes"){
-    var yearCC=parseInt(prompt("enter the first 2 digits of year you were born"));
-    var yearYY=parseInt(prompt("enter the last 2 digits of year you were born"));
-    var month=(prompt("enter the month you were born"));
+function data(){
+    var yearCC=parseInt(document.getElementById("yearCC").value);
+    var yearYY=parseInt(document.getElementById("yearYY").value);
+    var month=document.getElementById("month").value;
     var months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "november", "december"];
     var monthMM=parseInt(months.indexOf(month))+1;
-    var date=parseInt(prompt("enter the date you were born"));
+    var date=parseInt(document.getElementById("date").value);
 
     var day=function(yearYY, monthMM, date, yearCC){
         return -1*(((yearCC/4) -2*yearCC-1)+((5*yearYY/4))+((26*(monthMM+1)/10))+date)%7;
     };
     var result=parseInt(day(yearYY, monthMM, date, yearCC));
-    var answer=result.toFixed();
     var days=[ "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-    var actualDay=days[answer];
-    var outPut="you were born on "+ actualDay;
-    alert(outPut);
+    var actualDay=days[result];
+    alert("you were born on "+ actualDay)
 
     var gender=prompt("what's your gender?");
     if (gender==="male"){
         var namesBoys=["Kwame", "Kwasi", "Kwadwo","Kwabena", "Kwaku", "Yaw", "Kofi"];
-        var localName=namesBoys[answer];
+        var localName=namesBoys[result];
         var show="your Akan name would be "+localName;
             alert(show);
     }
     else if (gender==="female"){
         var namesGirls=["Ama", "Akosua", "Adwoa", "Abenaa", " Akua", "Yaa", "Afua"];
-        var nameLocal=namesGirls[answer];
+        var nameLocal=namesGirls[result];
         var showTwo="your Akan name would be "+nameLocal;
         alert(showTwo);
     }
 }
-else if(test==="no"){
-    alert("okay, later then");
-}
-else{
-    alert("invalid choice");
-}
-    
+
